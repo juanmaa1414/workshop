5352b3a1e9c2cb1762441ebbd6c35f3913f92a24
Juan Manuel Fernandez <sistemas@vapsistemas.com.ar>
2014-07-11 17:21:51 -0300

    Probando parte 8

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 var app = {
-
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
+    
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        this.store = new LocalStorageStore(function() {
-            
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
         });
-        
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        self.registerEvents();
     },
     
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
         } else {
-            alert(title ? (title + ": " + message) : message);
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
         }
     },
-    
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
 
 };
 
