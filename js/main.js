440f12cc6d4bee2fce39b6cd45b50de09123b9fe
Juan Manuel Fernandez <sistemas@vapsistemas.com.ar>
2014-07-11 12:15:51 -0300

    Probando parte 5

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
         var self = this;
         this.store = new LocalStorageStore(function() {
-            self.showAlert('Store initialized', 'Info');
+            this.homeTpl = Handlebars.compile($("#home-tpl").html());
+            this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         });
         
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
@@ -29,6 +24,11 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
+    
+    renderHomeView: function() {
+        $("body").html(this.homeTpl());
+        $(".search-key").on("keyup", $.proxy(this.findByName, this));
+    }
 
 };
 
